1ac1398b3e1663178d5ff16fb698cd3a19fc0ddb
Cedrik-Gagnon-Letarte_1642294 <1642294@cstj.qc.ca>
2021-11-03 18:02:29 -0400

    Créaton de Add/Teacher/Student, Probleme avec modifier donc j'ai créé moifier2, debut de la fenetre note.xaml

---
+++ Omnivox2/TP1_Base/Windows/AddTeacher.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TP1_Base_Prof.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour AddTeacher.xaml
+    /// </summary>
+    public partial class AddTeacher : Window
+    {
+        public AddTeacher()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
