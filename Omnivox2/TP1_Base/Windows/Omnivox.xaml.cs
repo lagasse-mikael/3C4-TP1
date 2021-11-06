52e3a9dcd94f5a8796c18cc25034ddc5f16f12fe
Risk1642294 <1642294@cstj.qc.ca>
2021-11-05 22:33:00 -0400

    Finalisation de note.xaml / Creation de omnivox ( presque terminer ) / Modifier fonctionne toujours pas et je sais pas pourquoi ... /

---
+++ Omnivox2/TP1_Base/Windows/Omnivox.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TP1_Base_Prof.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour Omnivox.xaml
+    /// </summary>
+    public partial class Omnivox : Window
+    {
+        public Omnivox()
+        {
+            InitializeComponent();
+        }
+    }
+}
